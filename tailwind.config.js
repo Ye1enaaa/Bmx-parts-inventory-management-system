fd9e534df66451816995816eaea185e3a422fd3c
joephinecalapiz <joephinecalapiz7@gmail.com>
2023-03-30 12:17:28 +0800

    customize the show.blade.php part

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+
+  ],
   theme: {
     extend: {},
   },

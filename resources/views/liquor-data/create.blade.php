e6a851b5881be6a7185ff9f260b0847edda14c27
joephinecalapiz <joephinecalapiz7@gmail.com>
2023-03-30 21:29:11 +0800

    testing4

--- resources/views/liquor-data/create.blade.php
+++ resources/views/liquor-data/create.blade.php
@@ -7,19 +7,47 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <title>Add Product</title>
 </head>
-<body>
-    <form action="{{route('post')}}" method="post">
+
+<body class="p-2 h-screen bg-gray-500 ">
+
+
+    <h1 class="flex text-4xl font-bold mb-10 text-white"><b>Add Products</b></h1>
+
+
+    
         
+    
+    <form action="{{route('post')}}" method="post">
         @csrf
-        <label for="">Name:</label>
-        <input type="text" class="form-control mb-3" name="name" required>
-        <label for="">Price:</label>
-        <input type="number" class="form-control mb-3" name="unit_price" required>
-        <label for="">Quantity:</label>
-        <input type="number" class="form-control mb-3" name="quantity" required>
-        <label for="">Description:</label>
-        <textarea name="description" class="form-control mb-3" cols="30" rows="5" required></textarea>
-        <button type="submit" class="btn btn-success col-md-3">Submit</button>
+
+        <div class="mb-6 ">
+            <label for=""class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name:</label>
+            <input type="text" class="form-control mb-3 bg-gray-50 border  text-gray-900 text-sm rounded-lg focus:border-blue-500 block py-3 px-20" name="name" required>
+        </div>
+
+
+
+        <label for=""class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Price:</label>
+        <input type="number" class="form-control mb-3 bg-gray-50 border  text-gray-900 text-sm rounded-lg focus:border-blue-500 block py-3 px-20" name="unit_price" required>
+
+        <label for=""class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Quantity:</label>
+        <input type="number" class="form-control mb-3 bg-gray-50 border  text-gray-900 text-sm rounded-lg focus:border-blue-500 block py-3 px-20" name="quantity" required>
+
+        <label for=""class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description:</label>
+        <textarea name="description" class="form-control mb-3 bg-gray-50 border  text-gray-900 text-sm rounded-lg focus:border-blue-500 block py-3 px-20" cols="30" rows="5" required></textarea>
+
+        <button type="submit" class="btn btn-success col-md-3 text-white bg-gray-900 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+
+        
+
+       
     </form>
+
+    </div>
+
+
+    
+
 </body>
+
 </html>
---
+++ resources/views/welcome1.blade.php
@@ -0,0 +1,99 @@
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=105">
+    <title>Generate Barcode In Laravel</title>       
+   <!-- Fonts -->
+   <!--<link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">-->
+   <!-- Bootstrap -->
+   <!--<link rel="stylesheet" href="https://cdn.tailwindcss.com">-->
+   <script src="https://cdn.tailwindcss.com"></script>
+   <!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css" integrity="sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==" crossorigin="anonymous" referrerpolicy="no-referrer" />-->
+
+  </head>
+
+
+  <body class="p-5 h-screen bg-gray-500">
+    <h1 class="text-3xl font-bold mb-5 text-white"><b>List of Products</b></h1>
+
+    <div class="pb-8 ">
+
+      <a href="/create" class="bg-cyan-900 text-white font-bold py-2 px-4 rounded hover: cyan-700">New Post</a>
+
+      </div>
+    
+        <div class="overflow-auto rounded-lg shadow hidden md:block">
+          
+              <table class="w-full whitespace-nowrap">
+                
+
+                  <thead class="text-white bg-gray-900 border-gray-900 " >
+                    
+                    <tr class="text-center font bold">
+                      <th class="border px-10  py-5 ">Product Code</th>
+                      <th class="border px-6  py-4 ">Title</th>
+                      <th class="border px-10  py-5 ">Price</th>
+                      <th class="border px-10  py-5 ">Quantity</th>
+                      <th class="border px-6  py-4 ">Qr Code</th>
+                      <th class="border px-6  py-4 ">Description</th>
+                    </tr>
+                  </thead>
+
+
+                  <tbody class="text-black text-center divide-y divide-gray-100">
+                    
+                    @foreach($product as $product)
+
+                    <tr class="bg-gray-200 hover:underline ">
+
+                        <td class="border px-6  py-4">{{$product->product_code}}</td>
+                        <td class="border px-6  py-4">{{$product->name}}</td>
+                        <td class="border px-6  py-4">{{$product->unit_price}}</td>
+                        <td class="border px-6  py-4">{{$product->quantity}}</td>
+                          
+                        <td class="border px-6  py-4"><img src="https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl={{$product->product_code}}"> </td>
+                        <td class="border px-6  py-4">{{$product->description}}</td>
+                    </tr>
+                    @endforeach
+                   
+
+                  </tbody>
+                  </div>    
+                </table>   
+                 
+        </div>
+
+        
+        <div class="grid grid-cols1 gap-4 md:hidden">
+          <div class="bg-white space-y-4 p-4 rounded-lg shadow">
+          <div class=flex items-center space-x-2 text-sm>
+            <div>
+              <div>
+                <a href="#"class="text-blue-500 font-bold hover:underline">{{$product->id}}</a>
+              </div>
+
+              <div class="text-gray-500">{{$product->name}}</div>
+              
+              <div class="text-gray-500">{{$product->unit_price}}</div>
+               
+              <div class="text-gray-500">img src="https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl={{$product->product_code}}</div>
+           
+
+              <div classtext-sm text-gray-700="">{{$product->description}}</div>
+            </div>
+
+          </div>
+          </div>
+
+
+        </div>
+
+      </div>
+
+    </div>
+   
+
+
+
+  </body>
+</html>
